ad36160bbce2c20982e3f436b977cfc3222ecdf0
bmanza <brent.manza@gmail.com>
2015-05-24 17:24:20 -0400

    upload

--- View/main.php
+++ View/main.php
@@ -1,5 +1,11 @@
 <?php require ('header.php'); ?>
 
 <h1>Welcome <?php echo $userName ?>!</h1>
+<br>
+
+<form action='uploade.php' method='post' enctype= 'multipart/form-data'>
+	<input type='file' name='file1'>
+	<br>
+	<input type='submit' value='upload'>
 
 <?php require ('footer.php'); ?>
